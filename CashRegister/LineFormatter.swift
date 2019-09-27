9962e57cd9da1e55b993d7c3660a4e7b7af09c06
Oleksiy Chebotarov <chebotarevalexei@gmail.com>
2019-09-27 14:32:51 +0300

    - Added LineFormatter tests

---
+++ CashRegister/LineFormatter.swift
@@ -0,0 +1,18 @@
+//
+//  LineFormatter.swift
+//  CashRegister
+//
+//  Created by Oleksiy Chebotarov on 27/09/2019.
+//  Copyright © 2019 Oleksiy Chebotarov. All rights reserved.
+//
+
+import Foundation
+
+class LineFormatter {
+
+    func addPaddingsAtEnd(to string: String, padding: Int) -> String {
+        let formatedString = string.padding(toLength: padding, withPad: " ", startingAt: 0)
+
+        return formatedString
+    }
+}
---
+++ CashRegisterTest/ForamtterTest.swift
@@ -0,0 +1,39 @@
+//
+//  ForamtterTest.swift
+//  CashRegisterTest
+//
+//  Created by Oleksiy Chebotarov on 27/09/2019.
+//  Copyright © 2019 Oleksiy Chebotarov. All rights reserved.
+//
+
+import XCTest
+
+class ForamtterTest: XCTestCase {
+    var formatter: LineFormatter!
+    
+    override func setUp() {
+        formatter = LineFormatter()
+    }
+
+    override func tearDown() {
+        formatter = nil
+    }
+
+    func testLineFormatterWithStringShouldReturntFormattedString() {
+        
+        let title = "Label of item"
+        let formatedTitle = formatter.addPaddingsAtEnd(to: title, padding: 15)
+        
+        XCTAssertEqual(formatedTitle, "Label of item  ")
+    }
+    
+    
+    func testLineFormatterShouldReturnFormattedArray() {
+        let titles = ["Label of item", "Quantity", "Unit price", "Total price"]
+        
+        let formatedHeaders = titles.map { formatter.addPaddingsAtEnd(to: $0, padding: 15) }
+        let expectedResult = ["Label of item  ", "Quantity       ", "Unit price     ", "Total price    "]
+        XCTAssertEqual(formatedHeaders, expectedResult)
+    }
+
+}
